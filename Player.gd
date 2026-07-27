d2068a23cb2ee14f146c8ae6ee2745e343ddd24d
Lan1 <olulani.o@gmail.com>
2026-07-27 21:50:02 +0100

    cd-5.5th-ia

--- Player.gd
+++ Player.gd
@@ -16,56 +16,42 @@ extends CharacterBody2D
 var facing: int = 1
 var fire_timer: float = 0.0
 var crouching: bool = false
-
-
-func _ready() -> void:
-	_configure_inputs()
+var jump_was_held: bool = false
+var fire_was_held: bool = false
 
 
 func _physics_process(delta: float) -> void:
 	if fire_timer > 0.0:
 		fire_timer = maxf(fire_timer - delta, 0.0)
 
-	var horizontal := Input.get_axis("move_left", "move_right")
-	if horizontal < 0.0:
-		facing = -1
-	elif horizontal > 0.0:
-		facing = 1
+	var left_held := Input.is_physical_key_pressed(KEY_W)
+	var right_held := Input.is_physical_key_pressed(KEY_D)
+	var horizontal := 0.0
+	if left_held != right_held:
+		horizontal = -1.0 if left_held else 1.0
+		facing = -1 if left_held else 1
 
-	crouching = Input.is_action_pressed("crouch")
+	crouching = Input.is_physical_key_pressed(KEY_S)
 	_update_crouch_state()
 	_update_muzzle_position()
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	var jump_held := Input.is_physical_key_pressed(KEY_SPACE)
+	if is_on_floor() and jump_held and not jump_was_held:
 		velocity.y = jump_velocity
+	jump_was_held = jump_held
 
 	var move_speed := crouch_speed if crouching else speed
 	velocity.x = horizontal * move_speed
 
-	if Input.is_action_just_pressed("fire"):
+	var fire_held := Input.is_physical_key_pressed(KEY_E)
+	if fire_held and not fire_was_held:
 		_try_fire()
+	fire_was_held = fire_held
 
 	move_and_slide()
-
-
-func _configure_inputs() -> void:
-	_bind_key("move_left", KEY_W)
-	_bind_key("move_right", KEY_D)
-	_bind_key("jump", KEY_SPACE)
-	_bind_key("crouch", KEY_S)
-	_bind_key("fire", KEY_E)
-
-
-func _bind_key(action_name: StringName, keycode: Key) -> void:
-	if not InputMap.has_action(action_name):
-		InputMap.add_action(action_name)
-	InputMap.action_erase_events(action_name)
-	var event := InputEventKey.new()
-	event.physical_keycode = keycode
-	InputMap.action_add_event(action_name, event)
 
 
 func _update_crouch_state() -> void:
